83388d4b572b3563c930c7032b1941efa8438d4f
Muhammad Zidane Akbar Ashshiddiq <zidanoke456@gmail.com>
2022-11-24 20:39:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello worlds<br>";
+
+$name = "zidane";
+$umur = 20;
+
+echo "Nama saya <strong>$name</strong>, saya berusia $umur tahun.<br>";
+
+
+$nameAdik = "Noval";
+$umurAdik = 17;
+
+echo "Nama adik saya <strong>$nameAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur-$umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.<br>"
+
+?>
